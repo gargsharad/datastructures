abd20d61d59e70a345a2206e47e183c4c5f6cdca
gargsharad <garg.sharadkumar@gmail.com>
2014-07-17 23:15:17 -0700

    Linked List

---
+++ src/com/sharad/ds/list/LinkedList.java
@@ -0,0 +1,17 @@
+package com.sharad.ds.list;
+
+
+public class LinkedList<E> {
+    
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+    
+}
